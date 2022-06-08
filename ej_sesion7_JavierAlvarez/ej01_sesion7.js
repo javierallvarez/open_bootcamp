2aa445e197d4e7c2dbfc5ba4cb0e68044b29bf81
Javier Allvarez <javiervarez@gmail.com>
2022-06-08 17:27:07 +0200

    ejercicios sets01 corregido

--- ej_sesion7_JavierAlvarez/ej01_sesion7.js
+++ ej_sesion7_JavierAlvarez/ej01_sesion7.js
@@ -2,10 +2,8 @@ const array = ['Piña', 'Coco', 'Totoro', 'Felpuíco', 'Rasputio', 'Javi']
 const mySet = new Set(array)
 console.log(mySet);
 
-// Incluye un nuevo valor:
 mySet.add('Javi')
 console.log(mySet);
 
-// No incluye un valor repetido:
 mySet.add('Javascript')
 console.log(mySet);

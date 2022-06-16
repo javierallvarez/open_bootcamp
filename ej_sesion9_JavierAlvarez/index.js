b5a8b41537d657d1f1ccd9b9eb86b3045105ff94
Javier Allvarez <javiervarez@gmail.com>
2022-06-16 13:46:07 +0200

    ej_sesion_9

---
+++ ej_sesion9_JavierAlvarez/index.js
@@ -0,0 +1,19 @@
+const logger = require('./logger')
+
+const miFunc = val => {
+    if (typeof val === "string"){
+    return val.toUpperCase()
+    }
+    throw new Error('El valor debe ser de tipo string')
+} 
+
+try {
+    const minus = miFunc(3)
+    console.log(minus);
+} catch(e) {
+    console.error(`${e}`);
+} finally {
+    console.log('Texto transformado a mayúsculas (solo en caso de introducir un string)');
+}
+
+logger.error('Mensaje de error');

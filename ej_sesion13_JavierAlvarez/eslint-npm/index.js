c769fd73229f1179a3ab03b0605dd6a0b0c05836
Javier Allvarez <javiervarez@gmail.com>
2022-06-20 13:35:42 +0200

    ej_sesion_13

---
+++ ej_sesion13_JavierAlvarez/eslint-npm/index.js
@@ -0,0 +1,14 @@
+let str_comillas = "Me dijo: 'Me gusta lo que haces', sonriendo.";
+console.log(str_comillas);
+
+let str_comillas_simples = "Me dijo: \"Me gusta lo que haces\", sonriendo.";
+console.log(str_comillas_simples);
+
+let str_backsticks = "Texto con backsticks";
+console.log(str_backsticks);
+
+
+let indentado = "Mal indentado";
+console.log(indentado);
+
+// corregido con lint-fix
